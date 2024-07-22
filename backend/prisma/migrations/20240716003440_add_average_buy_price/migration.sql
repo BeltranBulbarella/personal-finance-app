0638dfea575cc6fe5c2e0a2cb4b0ba0817ba920e
BeltranBulbarella <beltran.bulbarella@ing.austral.edu.ar>
2024-07-22 12:26:20 -0300

    added supabase

--- backend/prisma/migrations/20240716003440_add_average_buy_price/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `averageBuyPrice` to the `Holding` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Holding" ADD COLUMN     "averageBuyPrice" DOUBLE PRECISION NOT NULL;
--- backend/prisma/migrations/20240716004519_default_avgbuyprice/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Holding" ALTER COLUMN "averageBuyPrice" SET DEFAULT 0;
